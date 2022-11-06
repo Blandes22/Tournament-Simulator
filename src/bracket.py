630b58b0a4e924ae21e49d2932bcdd7ceda6a807
Blandes22 <96037855+Blandes22@users.noreply.github.com>
2022-11-06 13:38:45 -0600

    Updated file to have documentation

--- src/bracket.py
+++ src/bracket.py
@@ -1,4 +1,3 @@
-from tkinter import Place
 import numpy as np
 from tree import Tree
 from entrant import PlaceHolder
@@ -13,38 +12,60 @@ class Bracket:
         self.place_holder_tbd = PlaceHolder("TBD")
         self.__build_bracket()
 
-
+    # updates the values of the empty tree to match the values given in entrants
     def __build_bracket(self):
         self.tree.build_empty_tree(self.depth)
+        # finds how many empty nodes are on the bottom row of the bracket
         n = (2 ** (self.depth - 1))
+        # finds how many entrant nodes are on the bottom and how many need to be one tier up
+        # used to dynamically draw the bracket based on how many entrants there are
         self.remainder = n - self.size
-        level = 1
-        self.__traverse_bracket(self.tree.root, level)
-
+        self.__traverse_bracket(self.tree.root, 1)
+    
+    # sepcial traverse function that adds entrants to the bottom tier of the bracket
+    # this builds the bracket from right to left
     def __traverse_bracket(self, node, level):
+        # checks if bracket branch is at the max depth based on how many entrants need to be on an upper tier
         if (self.remainder > 0) and (level == self.depth - 1):
             self.remainder -= 1
+            # removes both node.left and node.right from current node
             node.left, node.right = None, None
+            # assigns the node an Entrant object as its value
             node.value = self.entrants.pop()
+        # checks if bracket branch is at the max depth if remainder == 0
         elif (level == self.depth):
             node.value = self.entrants.pop()
+        # if bracket branch is not at the max depth, set node.value to a placeholder and check right and left nodes
         else:
             node.value = self.place_holder_tbd
             self.__traverse_bracket(node.right, level + 1)
             self.__traverse_bracket(node.left, level + 1)
 
     def draw_bracket(self):
+        # see tree.py for this function
         self.tree.save_tree_as_array(self.tree.root)
+        # used to draw horizontal lines in bracket
         level = 0
+        # traverse trr_arr
         for i in self.tree.tree_arr:
+            # update level
             level += 1
+            # dynamically set max_line_size to fit up to 16 nodes from tree
             max_line_size = 11 * np.power(2, self.tree.depth - 1)
+            # dynamicallt set fill based on max line size
+            # fill is used to determine the space each TreeNode.value will have
             fill = int(max_line_size / np.power(2, level - 1))
+            # for all passes of the function except the first
             if level != 1:
+                # draw horizontal lines of bracket
                 print('\n', "|".center(fill) * len(i), sep="")
+            # traverse each index of tree_arr 
             for j in i:
+                # this checks if the current node is the at the bottom of its respective branch
                 if not j.left and not j.right: 
+                    # if so the only difference is that it will not use '_' to fill in the space
                     print(j.value.name.center(fill // 2 - 1, ' ').center(fill), end="")
                 else:
                     print(j.value.name.center(fill // 2 - 1, '_').center(fill), end="")
+        # before going into the next index of tree_arr, use a linebreak
         print('\n')
--- src/tree.py
+++ src/tree.py
@@ -1,3 +1,4 @@
+# undirected binary tree
 class TreeNode:
     def __init__(self, value):
         self.previous = None
@@ -5,38 +6,52 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# specific tree class for this project
 class Tree:
     def __init__(self):
         self.root = None
         self.depth = 0
         self.tree_arr = []
     
+    # builds an tree filled with nodes that have no value, previous is not called in initial calling of function
     def build_empty_tree(self, depth, previous=None):
+        # base case
         if depth == 0:
             return
+        # initialize empty node
         node = TreeNode(0)
+        # this will never be called on the first pass, ensuring that root has no previous node 
         if previous:
             node.previous = previous
+        # this will only be called on the first
         else:
+            # saves the current node as the root
             self.depth = depth
+            # sets tree's depth to the user requested depth
             self.root = node
+        # sets node.left and node.right to empty node if depth > 0
         node.left = self.build_empty_tree(depth - 1, node)  # type: ignore
         node.right = self.build_empty_tree(depth - 1, node) # type: ignore
         return node
     
+    # saves current tree as an array for accessibilty
     def save_tree_as_array(self, node):
+        # creats a list of size self.depth with each index holding an empty list
         self.tree_arr = [[] for i in range(self.depth)]
         self.__traverse_tree(node)
         return self.tree_arr
 
+    # traveses tree and saves each node to a 2d array for ease of access
     def __traverse_tree(self, node):
+        # this will find the negative index and work up to -1
+        # ex. if depth is 5, this will append a node to self.tree[-5]
+        # which if 5 is max depth, it will be the same as self.tree[0]
         self.tree_arr[self.depth - self.depth * 2].append(node)
+        # subtract 1 from self.depth to make sure the child nodes dont save in the same index
         self.depth -= 1
         if node.left:
             self.__traverse_tree(node.left)
         if node.right:
             self.__traverse_tree(node.right)
+        # add 1 to self.depth to make sure it has proper depth going back up the tree
         self.depth += 1
-        # this should be looking at nodes not the values at the nodes (NOTE COPIED IN "bracket.py")
-    
-# move functions outside of class
